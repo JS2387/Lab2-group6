5e8ebba197682f675d2a9adccdf89167e02a2712
JS2387 <jaskirat23387@gmail.com>
2021-11-14 00:52:53 +0100

    finished problem2

--- Problem2.R
+++ Problem2.R
@@ -28,15 +28,31 @@ optim_norm1
 mean(data)
 sd(data)
 
-optim_norm2 <- optim(par = c(0,1), loglikelihood, x = data, method = "BFGS")
-optim_norm2
 
 
-gradient <- function(x) {
-  
+gradient <- function(par, x) {
+  mu = par[1]
+  sigma = par[2]
+  n = length(x)
+  mu_est = -(sum(x) - n*mu)/sigma^2
+  sigma_est = n/sigma - sum((x - mu)^2)/sigma^3
+  return(c(mu_est, sigma_est))
 }
 
 
+optim_norm2 <- optim(par = c(0,1), gr = gradient, loglikelihood, x = data, method = "BFGS")
+optim_norm2
+
+optim_norm3 <- optim(par = c(0,1), gr = gradient, loglikelihood, x = data, method = "CG")
+optim_norm3
 
 
+optim_norm$par
+optim_norm1$par
+optim_norm2$par
+optim_norm3$par
 
+optim_norm$counts
+optim_norm1$counts
+optim_norm2$counts
+optim_norm3$counts
